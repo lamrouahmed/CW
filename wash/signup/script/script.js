b50d1620d51bb25ea5187e68b6a8a8be3910185d
lamrouahmed <lamrouahmed@gmail.com>
2020-04-10 13:18:26 +0100

    separated the signup form fo wash

--- wash/signup/script/script.js
+++ wash/signup/script/script.js
@@ -4,6 +4,12 @@ const labels = $$('.label');
 const spans = $$('.border');
 const hamburger = $('.hamburger');
 const nav = $('.nav');
+const next = $('.next a');
+const parts = $$('.form > div');
+const [part1, part2] = parts;
+
+
+next.addEventListener('click', nextStep);
 
 labels.forEach((label, index) => label.addEventListener('click', () => addBorder(index)))
 
@@ -18,5 +24,11 @@ function removeBorder(e, node) {
 function addBorder(index) {
     spans.forEach(span => span.classList.remove('clicked'))
     spans[index].classList.add('clicked');
+}
 
+function nextStep() {
+    console.log(part1, part2);
+
+    part1.style.display = "none";
+    part2.style.display = "block";
 }
