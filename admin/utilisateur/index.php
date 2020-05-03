532142f77eca7286034279343d90473561435e47
lamrouahmed <lamrouahmed@gmail.com>
2020-05-03 16:45:08 +0000

    60% of the user crud in done

--- admin/utilisateur/index.php
+++ admin/utilisateur/index.php
@@ -1,3 +1,73 @@
 <?php
+require_once '/wamp64/www/PFE/core/init.php';
 require_once '/wamp64/www/PFE/admin/sideBar/sideBar.html';
+
+$DB = DB::connect();
+$results = $DB->getAll("user")->results();
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="top">
+            <div class="id">#</div>
+            <div class="uid">username</div>
+            <div class="id">Full Name</div>
+            <div class="tel">Phone</div>
+            <div class="mail">Email</div>
+            <div class="joined">Created</div>
+            <div class="action">action</div>
+        </div>
+
+        <div class="content">
+            <?php foreach($results as $result) {
+
+            ?>
+            <div class="mid">
+                <div class="p_id"><?php echo $result->u_id; ?></div>
+                <div class="p_uid"><?php echo $result->username ?></div>
+                <div class="p_name"><?php echo $result->last_name. " " . $result->first_name ?></div>
+                <div class="p_tel"><?php echo $result->phone ?></div>
+                <div class="p_mail"><?php echo $result->mail?></div>
+                <div class="p_joined"><?php echo $result->created?></div>
+                <div class="action">
+                    <a href="#" >
+                        <svg class="delete" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 384 384"
+                                style="enable-background:new 0 0 384 384;" xml:space="preserve">
+
+                                <path
+                                    d="M64,341.333C64,364.907,83.093,384,106.667,384h170.667C300.907,384,320,364.907,320,341.333v-256H64V341.333z" />
+                                <polygon
+                                    points="266.667,21.333 245.333,0 138.667,0 117.333,21.333 42.667,21.333 42.667,64 341.333,64 341.333,21.333 			" />
+                        </a>
+                        <a href="#" >
+                            <svg class="update" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 383.947 383.947"
+                                style="enable-background:new 0 0 383.947 383.947;" xml:space="preserve">
+                                <polygon points="0,303.947 0,383.947 80,383.947 316.053,147.893 236.053,67.893 			" />
+                                <path d="M377.707,56.053L327.893,6.24c-8.32-8.32-21.867-8.32-30.187,0l-39.04,39.04l80,80l39.04-39.04
+                    C386.027,77.92,386.027,64.373,377.707,56.053z" />
+                            </svg>
+                            </a>
+                </div>
+            </div>
+         <?php } ?>
+       
+
+
+        </div>
+    </div>
+</body>
+
+</html>
