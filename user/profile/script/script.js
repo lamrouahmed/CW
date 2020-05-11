36e8046a68332bb65f751a1914f163e5f3e6706d
lamrouahmed <lamrouahmed@gmail.com>
2020-05-11 16:59:52 +0000

    30% of the profil is done

--- user/profile/script/script.js
+++ user/profile/script/script.js
@@ -3,8 +3,23 @@ const $$ = e => document.querySelectorAll(e)
 
 
 const triangle = $('.triangle');
+const inputs = $$('.input');
 
 triangle.addEventListener('click', () => {
     $('.triangle svg').classList.toggle('svgClicked')
     $('.logout').classList.toggle('displayLog')
+})
+
+inputs.forEach((input, index) => input.addEventListener('focus', () => {
+    $$('.text')[index].classList.add('focus');
+    $$('.border')[index].classList.add('clicked')
+}))
+
+inputs.forEach((input, index) => input.addEventListener('blur', e => {
+    e.currentTarget.value.trim() === '' &&  $$('.text')[index].classList.remove('focus');
+    $$('.border')[index].classList.remove('clicked')
+}))
+
+inputs.forEach((input, index) => {
+    input.value.trim() === '' ?  $$('.text')[index].classList.remove('focus') : $$('.text')[index].classList.add('focus');
 })
