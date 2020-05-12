78f4efa3040531104cb2f8c7aead4813745dde76
lamrouahmed <lamrouahmed@gmail.com>
2020-05-12 15:52:57 +0000

    some css

--- user/profile/script/script.js
+++ user/profile/script/script.js
@@ -4,6 +4,7 @@ const $$ = e => document.querySelectorAll(e)
 
 const triangle = $('.triangle');
 const inputs = $$('.input');
+const show = $('.show')
 
 triangle.addEventListener('click', () => {
     $('.triangle svg').classList.toggle('svgClicked')
@@ -13,6 +14,7 @@ triangle.addEventListener('click', () => {
 inputs.forEach((input, index) => input.addEventListener('focus', e => {
     $$('.text')[index].classList.add('focus');
     $$('.border')[index].classList.add('clicked')
+    e.currentTarget.setSelectionRange(0,e.currentTarget.value.length); 
     if(e.currentTarget.classList.contains('textarea') && e.currentTarget.value.trim() === "") {
         e.currentTarget.setSelectionRange(0,0); 
     }
@@ -25,4 +27,11 @@ inputs.forEach((input, index) => input.addEventListener('blur', e => {
 
 inputs.forEach((input, index) => {
     input.value.trim() === '' ?  $$('.text')[index].classList.remove('focus') : $$('.text')[index].classList.add('focus');
+})
+
+
+
+show.addEventListener('click', e => {
+    e.currentTarget.classList.toggle('hide')
+    e.currentTarget.classList.contains('hide') ? $('.pwd').setAttribute('type', 'text') : $('.pwd').setAttribute('type', 'password')
 })
