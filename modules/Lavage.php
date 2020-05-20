db52c1cea85c13b17bec4174e78881ff148b5fc3
Hanane-zyx <hanane.zyx@gmail.com>
2020-05-20 21:10:49 +0000

    Added form's related files

---
+++ modules/Lavage.php
@@ -0,0 +1,28 @@
+<?php
+    
+    class Lavage {
+
+        private $db;
+        public function __construct($lavage = null)
+        {
+            $this->db = DB::connect();
+        }
+
+        public function create($table, $fields) 
+        {
+            //  if(!$this->db->insert($table, $fields)) {
+            //      throw new Exception("something went wrong during the creation of an account");
+            //  } else {
+            //      echo "inserted";
+            //  }
+
+             $this->db->insert($table, $fields);
+        }
+
+        public function update($PK,  $id, string $table, $params = []) {
+            $this->db->update($PK, $id, $table, $params);
+        }
+
+}
+
+
