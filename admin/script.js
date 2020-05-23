219e65d704cfcdfd5e6a417c22225ffb08b3de8a
lamrouahmed <lamrouahmed@gmail.com>
2020-05-23 00:32:42 +0000

    a_form

---
+++ admin/script.js
@@ -0,0 +1,27 @@
+const $ = e => document.querySelector(e)
+const $$ = e => document.querySelectorAll(e)
+
+const inputs = $$('.input');
+
+
+
+inputs.forEach((input, index) => input.addEventListener('focus', e => {
+    $$('.text')[index].classList.add('focus');
+    $$('.border')[index].classList.add('clicked')
+    e.currentTarget.type !== "password" && e.currentTarget.setSelectionRange(0,e.currentTarget.value.length); 
+    if(e.currentTarget.classList.contains('textarea') && e.currentTarget.value.trim() === "") {
+        e.currentTarget.setSelectionRange(0,0); 
+    }
+
+
+}))  
+
+
+inputs.forEach((input, index) => input.addEventListener('blur', e => {
+    e.currentTarget.value.trim() === '' &&  $$('.text')[index].classList.remove('focus');
+    $$('.border')[index].classList.remove('clicked')
+}))
+
+inputs.forEach((input, index) => {
+    input.value.trim() === '' ?  $$('.text')[index].classList.remove('focus') : $$('.text')[index].classList.add('focus');
+})
