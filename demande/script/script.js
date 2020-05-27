7f0536670964dda0c5b49e4381c562d0a896c916
lamrouahmed <lamrouahmed@gmail.com>
2020-05-27 00:53:50 +0000

    check_css

--- demande/script/script.js
+++ demande/script/script.js
@@ -2,6 +2,29 @@ const $ = e => document.querySelector(e)
 const $$ = e => document.querySelectorAll(e)
 
 const input = $('.input');
+const lavage = $$('input[name=lavage]');
+const vehicule = $$('input[name=vehicule]');
+const quantityModif = $('.btn');
+console.log(quantityModif, vehicule, lavage);
+
+
+lavage.forEach((btn, i) => {
+    btn.checked && $$('.lavageLabel')[i].classList.add('checked');
+
+    btn.addEventListener('click', () => {
+        $$('.lavageLabel').forEach(label => label.classList.remove('checked'));
+        btn.checked && $$('.lavageLabel')[i].classList.add('checked');
+    })
+})
+vehicule.forEach((btn, i) => {
+    btn.checked && $$('.vehiculeLabel')[i].classList.add('checked');
+
+    btn.addEventListener('click', () => {
+        $$('.vehiculeLabel').forEach(label => label.classList.remove('checked'));
+        btn.checked && $$('.vehiculeLabel')[i].classList.add('checked');
+    })
+})
+
 
 
 input.addEventListener('focus', e => {
@@ -14,4 +37,3 @@ input.addEventListener('blur', e => {
 })
 
 
-console.log('ze');
