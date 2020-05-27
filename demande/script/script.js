7becdbacdf23f7242112c596a5dec143e5fdae9f
lamrouahmed <lamrouahmed@gmail.com>
2020-05-27 01:16:35 +0000

    +-

--- demande/script/script.js
+++ demande/script/script.js
@@ -4,9 +4,18 @@ const $$ = e => document.querySelectorAll(e)
 const input = $('.input');
 const lavage = $$('input[name=lavage]');
 const vehicule = $$('input[name=vehicule]');
-const quantityModif = $('.btn');
-console.log(quantityModif, vehicule, lavage);
+const quantityModif = $$('.btn');
 
+
+quantityModif.forEach(btn => btn.addEventListener('click', e => modify(e, $('.quantite'))));
+
+function modify(e, node) {
+        const element = e.currentTarget;
+        (element.dataset.action === '-' && node.value--) && ($('.btn[data-action="+"]').style.opacity="1");
+        (element.dataset.action === '+' && node.value++) && ($('.btn[data-action="-"]').style.opacity="1");
+        (node.value >= 9 && (node.value = 9)) && ($('.btn[data-action="+"]').style.opacity=".4");
+        (node.value <= 1 && (node.value = 1)) && ($('.btn[data-action="-"]').style.opacity=".4");
+}
 
 lavage.forEach((btn, i) => {
     btn.checked && $$('.lavageLabel')[i].classList.add('checked');
