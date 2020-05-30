c8bd1bcad19e7f37e715c37649673b6264ef6fff
lamrouahmed <lamrouahmed@gmail.com>
2020-05-30 18:18:02 +0000

    a_users_check

--- admin/utilisateur/index.php
+++ admin/utilisateur/index.php
@@ -85,8 +85,16 @@ if($action === "delete") {
         </div>
 
         <div class="top">
-            <div class="void"></div>
-            <div class="id">#</div>
+            <!-- <div class="id">#</div> -->
+            <div class="checkWrapper">
+            <input type="checkbox" name="check" class="parentInput" id="CHECK">
+<label for="CHECK">
+<svg viewBox="0 0 100 100" class="checkbox">
+	            <path class="box" d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z"/>
+                <polyline class="check" points="25.5,53.5 39.5,67.5 72.5,34.5 "/>
+            </svg>
+</label>
+            </div>
             <div class="uid">username</div>
             <div class="id">Full Name</div>
             <div class="tel">Phone</div>
