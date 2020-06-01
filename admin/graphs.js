e81a7c27e871f0fca87649bcfb989e1cec797856
lamrouahmed <lamrouahmed@gmail.com>
2020-06-01 02:10:51 +0100

    a_charts

--- admin/graphs.js
+++ admin/graphs.js
@@ -1,18 +1,56 @@
 const $ = e => document.querySelector(e);
 const $$ = e => document.querySelectorAll(e);
+const days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+const uStatus = ['offline', 'online']
+const globalData = {
+    users: {
+        labels: days,
+        datasets: [{
+            label: 'Nombre des comptes cree par jour',
+            backgroundColor: 'rgba(234, 106, 134, 0.16)', 
+            borderColor: 'rgb(255, 99, 132)',
+            data: []
+        }]
+    },
+    demandes: {
+        labels: days,
+        datasets: [{
+            label: 'Nombre de demandes par jour',
+            backgroundColor: '#f6f8fd',
+            borderColor: '#376ade',
+            data: []
+        }]
+    },
+    status: {
+        labels: uStatus,
+        datasets: [{
+            data: [],
+            backgroundColor: [
+                "#ff6384",
+                "#4bc0c0"
+            ]
+        }]  
+    }
+}
+
+
+const d_chart = createChart($('#d_chart'), 'line', globalData.demandes);
+const u_chart = createChart($('#u_chart'), 'line', globalData.users);
+const status_chart = createChart($('#pie'), 'doughnut', globalData.status);
 
 get('/PFE/admin/data.php');
 
+setInterval(() => get('/PFE/admin/data.php'), 1000)
 
 function get(url) {
     fetch(url, {
             method: 'get'
         })
         .then(response => response.json())
-        .then(body => createChart(body));
+        .then(body => updateChart(body));
 }
 
-function createChart(data) {
+function updateChart(data) {
     const {users,demandes} = data;
     let u_creationDates = users.map(user => getDayName(new Date(user.created), 'fr-FR'));
     let records = {
@@ -39,7 +77,6 @@ function createChart(data) {
             online: 0
         }
     }
-    days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
     days.forEach(dateP => {
         u_creationDates.forEach(dateC => {
             dateP === dateC && records.users[dateP]++
@@ -47,52 +84,14 @@ function createChart(data) {
     });
     let U = [];
     days.forEach(day => U.push(records.users[day]));
-    const usersChart = $('#u_chart').getContext('2d');
-    const u_chart = new Chart(usersChart, {
-
-        type: 'line',
-
-        data: {
-            labels: days,
-            datasets: [{
-                label: 'Nombre des comptes cree par jour',
-                backgroundColor: 'rgba(234, 106, 134, 0.16)', 
-                borderColor: 'rgb(255, 99, 132)',
-                data: U
-            }]
-        },
-        responsive: true,
-        options: {}
-    });
-    const pie = $('#pie').getContext('2d');
+    update(u_chart, U);
     users.forEach(user => {
         user.status === "online" && records.status.online++;
         user.status === "offline" && records.status.offline++;
     })
-    const status_chart = new Chart(pie, {
-        type: 'doughnut',
-        data : {
-            datasets: [{
-                data: [records.status.offline, records.status.online],
-                backgroundColor: [
-                    "#ff6384",
-                    "#4bc0c0"
-                ]
-            }],
-
-            labels: [
-                'Offline',
-                'Online'
-            ]
-        },
-        options: {
-
-        }
-    })
 
 
-
-
+    update(status_chart, [records.status.offline, records.status.online]);
 
 
 
@@ -105,7 +104,6 @@ function createChart(data) {
 
 
     let d_creationDates = demandes.map(demande => getDayName(new Date(demande.date_demande), 'fr-FR'));
-    const demandesChart = $('#d_chart').getContext('2d');
 
     days.forEach(dateP => {
         d_creationDates.forEach(dateC => {
@@ -114,27 +112,11 @@ function createChart(data) {
     });
     let D = [];
     days.forEach(day => D.push(records.demandes[day]));
-    const d_chart = new Chart(demandesChart, {
-
-        type: 'line',
-
-        data: {
-            labels: days,
-            datasets: [{
-                label: 'Nombre de demandes par jour',
-                backgroundColor: '#f6f8fd',
-                borderColor: '#376ade',
-                data: D
-            }]
-        },
-        responsive: true,
-        options: {}
-    });
+    update(d_chart, D);
 
 
 
 
-    
 
 
 }
@@ -149,18 +131,25 @@ function getDayName(date, locale) {
 
 
 
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
+
+
+
+
+function createChart(node, type, newData) {
+    const status_chart = new Chart(node, {
+        type: type,
+        data : newData
+    })
+    return status_chart;
 }
 
-function removeData(chart) {
-    chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
+
+
+
+
+
+
+function update(chart, newData) {
+    chart.data.datasets[0].data = newData;
     chart.update();
 }
