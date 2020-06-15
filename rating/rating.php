dcfe6fcabd6dd8f8a05caaf92aa195278fc49771
assaad-amine <60930997+assaad-amine@users.noreply.github.com>
2020-06-15 06:30:41 +0100

    Add files via upload
    don't forget to modify the index file  <li class="navItem"><a href="\PFE\rating\rating.int.php">Reviews</a></li>
    i will out the DB latter or u can just add rating "int" and feedback "varchar"

--- rating/rating.php
+++ rating/rating.php
@@ -1,34 +1,51 @@
 <?php
-$mysqli = new mysqli('127.0.0.1','root','','carwash');
-if(isset($_POST['rating'])){
-    $email = $_POST['email'];
+    require_once '/wamp64/www/PFE/core/init.php';
+error_reporting(0);
+
+
+
+    if(Session::exists("user")) { 
+        $user = new User();
+        
+?>
+
+
+
+<?php
+$mysqli = mysqli_connect ('127.0.0.1','root','','carwash');
+
+if(isset( $_POST['rating'] )){
     $rating =$_POST['rating'];
     $feedback =$_POST['feedback'];
     
     $errors = array();
 
     if(empty($rating)){
-        $errors['r'] = "Etoile Nécessaire";
+        $errors['rating'] = "Etoile Nécessaire";
     }
 
-    if(empty($email)){
-        $errors['e'] = "Email Nécessaire" ;
-    }
+  /*  if(empty($mail)){
+        $errors['mail'] = "Email Nécessaire" ;
+    }*/
 
     if (count($errors)==0){
-        $query = "insert into feedback (rating,feedback,email) VALUES(?,?,?) ";
-        $stmt = $mysqli->prepare($query);
-        $stmt->bind_param('iss',$rating,$feedback , $email );
+        if(isset($_POST['update'])){
+       //$user = new User(); where u_id = ".$user->getData()->u_id."
+        $id = $user->getData()->u_id  ;
+        $query = " UPDATE user set rating = '$_POST[rating]' , feedback ='$_POST[feedback]' where u_id = '$id' " ;
+        $stmt = $mysqli->prepare($query);  
+        //$stmt->bind_param( $rating, $feedback );
         $stmt->execute();
         $msg="<div class='alert alert-success'> Rating Successfully Added </div>";
-        $stmt->close();
+        //$stmt->close();
+        }
     }
     
 }
 
     function getAverageRating(){
         global $mysqli;
-        $query = "select avg(rating) as avg from feedback";
+        $query = "select avg(rating) as avg from user where rating >0";
         $stmt = $mysqli->prepare($query);
         if($stmt->execute()){
         $result = $stmt->get_result();
@@ -53,10 +70,12 @@ if(isset($_POST['rating'])){
 
     <title>rating</title>
 
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
      crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+    <link rel="stylesheet" href="./css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 
@@ -65,30 +84,30 @@ if(isset($_POST['rating'])){
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <br><br>
-                    <h2>Merci de donner votre avis</h2> <hr>
+                    <h2>Merci de donner votre avis <?php echo $user->getData()->username  ; ?> </h2> <hr>
 
                         <?php 
                             if(isset($msg)){echo $msg;}
 
                         ?>
 
-                <form action="" method="post" >
+                <form action="" method="post" class="form" >
                    <div class="form-group">
                       <label for="">Rating</label>
                         <div id="rateYo"></div>
                     </div>
-                    <p class="text-danger"><?php if(isset($errors['r'])) echo $errors['r']; ?></p>
-                    <div class="form-group">
-                        <label for="">Email</label>
-                      <input type="email" name="email" id="" class="form-control" placeholder="entrer votre adress mail"  value="" >
-                      <p class="text-danger"><?php if(isset($errors['e'])) echo $errors['e']; ?></p>
-                    </div>
+                    <p class="text-danger"><?php if(isset($errors['rating'])) echo $errors['rating']; ?></p>
+             <!--       <div class="form-group">
+                     <label class="label"></label>
+                      <input type="email" name="mail" id="" class="input" placeholder="Email"  value="" >
+                      <p class="text-danger">   <?php // if(isset($errors['mail'])) echo $errors['mail']; ?></p>
+                    </div>   -->
                    <div class="form-group">
-                    <label for="">Feedback/Commentaire</label>
-                      <input type="text" name="feedback" class="form-control">
+                    <label class="label"></label>
+                      <input type="text" name="feedback" class="input" placeholder="Feedback/Commentaire">
                       <input type="hidden" name="rating" id="rating">
                     </div>
-                    <button class="btn btn-primary">Submit</button>               
+                    <button name="update" class="btn btn-primary">Submit</button>               
                 </form>
                 <hr>
                 <h2>Users Feedback</h2>
@@ -97,7 +116,7 @@ if(isset($_POST['rating'])){
 </script>
                 <hr>
                 <?php
-                $query = "select * from feedback";
+                $query = "SELECT * from user where rating>=3 order by rand() limit 3";
                 $stmt = $mysqli->prepare($query);
                 if($stmt->execute()){
                     $result = $stmt->get_result();
@@ -107,15 +126,15 @@ if(isset($_POST['rating'])){
                             <div class="media">
                             <div class="media-left">
                                 <a href="#">
-                                <img class="media-object" src="index.png" alt="" height="64px" width="64pix" >
+                                <img class="media-object" src="/PFE/uploads/<?php echo $row['img']?>" alt="img" height="64px" width="64pix" >
                                 </a>
                             </div>
                             <div class="media-body">
-                                <h4 class="media-heading"> <div class="rateYo-<?php echo $row['rating_id'];?>" > </div> </h4>
+                                <h4 class="media-heading"> <div class="rateYo-<?php echo $row['u_id'];?>" > </div> </h4>
                                 <script>
                                         $(function () {
                                     
-                                        $(".rateYo-<?php echo $row['rating_id'];?>").rateYo({
+                                        $(".rateYo-<?php echo $row['u_id'];?>").rateYo({
                                             readOnly:true,
                                             rating:<?php echo $row['rating'];?>,
                                         });
@@ -124,7 +143,7 @@ if(isset($_POST['rating'])){
                                     </script>
                                 <?php echo $row['feedback']; ?> 
                                 <br>
-                                by: <i><?php echo $row['email']; ?></i>
+                                by: <i><?php echo $row['username']; ?></i>
                                 
                             </div>
                             </div>
@@ -166,3 +185,9 @@ crossorigin="anonymous"></script>
 
 </body>
 </html>
+
+
+    <?php } else {
+        Redirect::to("/PFE/user/login/login.php");
+    }
+?>
