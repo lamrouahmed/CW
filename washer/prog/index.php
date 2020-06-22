e8dc5efae2a9ff591cf81afce4dd5ec8338eadf7
Hanane-zyx <hanane.zyx@gmail.com>
2020-06-22 20:20:27 +0100

    lavages

---
+++ washer/prog/index.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../../core/init.php';
+require_once '../sideBar/sideBar.php';
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Progress</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+
+<ul class="steps">
+  <li class="step step--incomplete step--active">
+    <span class="step__icon"></span>
+    <span class="step__label">Arrivé</span>
+    
+  </li>
+  <li class="step step--incomplete step--inactive">
+    <span class="step__icon"></span>
+    <span class="step__label">Voiture en main</span>
+  </li>
+  <li class="step step--incomplete step--inactive">
+    <span class="step__icon"></span>
+    <span class="step__label">Fin lavage</span>
+  </li>
+  <li class="step step--incomplete step--inactive">
+    <span class="step__icon"></span>
+    <span class="step__label">Voiture rendue</span>
+  </li>
+</ul>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script  src="./script.js"></script>
+
+</body>
+</html>
