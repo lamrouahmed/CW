a1c1d0bb723daff4fb173bfbaa1ad828d9966473
lamrouahmed <lamrouahmed@gmail.com>
2020-06-25 19:12:14 +0100

    payment_css

--- payment/payment.php
+++ payment/payment.php
@@ -33,10 +33,8 @@
             </div>
         </a>
     </div>
-<div class="payment-title">
-        <h1>Payment Information</h1>
-    </div>
-    <div class="container preload">
+
+    <!-- <div class="container preload">
         <div class="creditcard">
             <div class="front">
                 <div id="ccsingle"></div>
@@ -133,40 +131,172 @@
                 </svg>
             </div>
         </div>
-    </div>
-    <form class="form-container form" method="POST" action=<?php echo escape($_SERVER["PHP_SELF"]);?>>
+    </div> -->
 
-        <label class="label">
-            <input data-check="u_last_name" id="name" maxlength="20" class="input" type="text" name="u_last_name" value="<?php echo $user->getData()->last_name?>">
-            <span class="border"></span>
-            <span class="text">Name</span>
-            <span class="error" data-error="u_last_name"></span>
-        </label>
-        <label class="label cardNumber">
-            <input data-check="u_card_number" id="cardnumber" class="input" type="text" pattern="[0-9]*" inputmode="numeric"  name="u_card_number">
-            <span class="border"></span>
-            <span class="text">Card Number</span>
-            <span class="error" data-error="u_card_number"></span>
-            <svg id="ccicon" class="ccicon" width="750" height="471" viewBox="0 0 750 471" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <main class="main">
+        <section class="section_1">
+            <div class="titre">
+               
+                <div class="img">
+                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 509.287 509.287" style="enable-background:new 0 0 509.287 509.287;" xml:space="preserve">
+                        <circle style="fill:#4F5565;" cx="254.644" cy="254.644" r="254.644"/>
+                        <path style="fill:#DEDEDF;" d="M482.84,286.517H409.6v144.106c0,20.344,16.276,36.62,36.62,36.62l0,0  c20.344,0,36.62-16.276,36.62-36.62V286.517z"/>
+                        <path style="fill:#F1F3F7;" d="M373.319,42.384c20.344,0,36.62,16.276,36.62,36.62v207.513v144.106  c0,20.344,16.276,36.62,36.62,36.62H143.428c-20.344,0-36.62-16.276-36.62-36.62V42.384H373.319z"/>
+                        <path style="fill:#FCFCFD;" d="M63.068,42.384h310.252c-20.344,0-36.62,16.276-36.62,36.62v7.121H99.348H26.448v-7.121  C26.448,58.66,42.723,42.384,63.068,42.384z"/>
+                        <g>
+                            <rect x="149.87" y="134.612" style="fill:#DEDEDF;" width="215.65" height="14.919"/>
+                            <rect x="149.87" y="180.726" style="fill:#DEDEDF;" width="215.65" height="14.919"/>
+                            <rect x="149.87" y="226.84" style="fill:#DEDEDF;" width="215.65" height="14.919"/>
+                            <rect x="246.167" y="272.954" style="fill:#DEDEDF;" width="119.354" height="14.919"/>
+                            <rect x="246.167" y="319.068" style="fill:#DEDEDF;" width="119.354" height="14.919"/>
+                            <rect x="246.167" y="365.521" style="fill:#DEDEDF;" width="119.354" height="14.919"/>
+                            <rect x="246.167" y="411.634" style="fill:#DEDEDF;" width="57.981" height="14.919"/>
+                        </g>
+                        <path style="fill:#FBD303;" d="M221.075,375.354c0-19.666-15.936-35.603-35.603-35.603c-8.477,0-15.258-6.781-15.258-15.258  c0-8.477,6.781-15.258,15.258-15.258c4.408,0,8.138,1.695,11.189,4.747c3.73,4.069,10.172,4.408,14.241,0.339  c4.069-3.73,4.408-10.172,0.339-14.241c-4.408-4.747-9.833-7.799-15.597-9.833v-7.121c0-5.764-4.408-10.172-10.172-10.172  s-10.172,4.408-10.172,10.172v7.121c-14.58,4.408-25.43,17.971-25.43,34.246c0,19.666,15.936,35.603,35.603,35.603  c8.477,0,15.258,6.781,15.258,15.258c0,8.477-6.781,15.258-15.258,15.258c-4.408,0-8.138-1.695-11.189-4.747  c-3.73-4.069-10.172-4.408-14.241-0.339c-4.069,3.73-4.408,10.172-0.339,14.241c4.408,4.747,9.833,7.799,15.597,9.833v7.121  c0,5.764,4.408,10.172,10.172,10.172s10.172-4.408,10.172-10.172V409.6C210.225,404.853,221.075,391.29,221.075,375.354z"/>
+                        
+                        </svg>
+                </div>
+                <h1>
+                    Detail de payment
+                </h1>
+            </div>
+                <div class="userInfo">
+                    <div class="fullName">
+                        <div class="top">
+                            <p>
+                                Patient Name
+                            </p>
+                        </div>
+                        <div class="bottom">
+                            <p>
+                                Lamrouah Mohamed
+                            </p>
+                        </div>
+                    </div>
+                    <div class="localisation">
+                        <div class="top">
+                            <p>
+                                Localisation
+                            </p>
+                        </div>
+                        <div class="bottom">
+                            <p>
+                                Lorem ipsum dolor sit dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="lavageVehicule">
+                        <div class="typeLavage">
+                            <div class="top">
+                                <p>
+                                    Type Lavage
+                                </p>
+                            </div>
+                            <div class="bottom">
+                                <p>
+                                    Pro
+                                </p>
+                            </div>
+                        </div>
+                        <div class="typeVehicule">
+                            <div class="top">
+                                <p>
+                                    Type Vehicule
+                                </p>
+                            </div>
+                            <div class="bottom">
+                                <p>
+                                    Camion
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mailPhone">
+                        <div class="mail">
+                            <div class="top">
+                                <p>
+                                    Adresse Mail
+                                </p>
+                            </div>
+                            <div class="bottom">
+                                <p>
+                                    lamrouahmed@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                        <div class="phone">
+                            <div class="top">
+                                <p>
+                                    Telephone
+                                </p>
+                            </div>
+                            <div class="bottom">
+                                <p>
+                                    06-77-77-89-89
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            
 
-            </svg>
-        </label>
-        <div class="bottom">   
-            <label class="label">
-                <input data-check="u_expiration" id="expirationdate" class="input" type="text" pattern="[0-9]*" inputmode="numeric" name="u_expiration">
-                <span class="border"></span>
-                <span class="text">Expiration (mm/yy)</span>
-                <span class="error" data-error="u_expiration"></span>
-            </label>
-            <label class="label">
-                <input data-check="u_cvv" id="securitycode" class="input" type="text" pattern="[0-9]*" inputmode="numeric" name="u_cvv">
-                <span class="border"></span>
-                <span class="text">Security Code</span>
-                <span class="error" data-error="u_cvv"></span>
-            </label>    
-        </div>
-            <button class="log">Pay</button>
-    </form>
+                <div class="paimentDetail">
+                    <div class="texte">
+                        <p>Total</p>
+                    </div>
+                    <div class="total">
+                        <p>635 M.A.D</p>
+                    </div>
+                    
+                </div>
+        </section>
+        <section class="section_2">
+            <div class="formWrapper">
+                <div class="formHeader">
+                    <div class="logo">
+                        <svg id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><g><path d="m466 80h-420c-25.36 0-46 20.64-46 46v260c0 25.36 20.64 46 46 46h420c25.36 0 46-20.64 46-46v-260c0-25.36-20.64-46-46-46z" fill="#e2e0e0"/><path d="m466 80h-32c25.36 0 46 20.64 46 46v260c0 25.36-20.64 46-46 46h32c25.36 0 46-20.64 46-46v-260c0-25.36-20.64-46-46-46z" fill="#cecccc"/><path d="m365.699 301.71c-3.468 0-6.96-1.122-9.895-3.435-6.939-5.471-8.131-15.531-2.66-22.471 4.486-5.688 6.856-12.537 6.856-19.804s-2.37-14.116-6.855-19.805c-5.471-6.939-4.279-17 2.66-22.471 6.939-5.469 17-4.28 22.471 2.66 8.85 11.228 13.724 25.297 13.724 39.616s-4.874 28.388-13.725 39.615c-3.157 4.005-7.843 6.095-12.576 6.095z" fill="#98d7ce"/><path d="m405.425 341.43c-3.807 0-7.626-1.35-10.682-4.092-6.576-5.901-7.124-16.018-1.222-22.594 14.496-16.154 22.479-37.016 22.479-58.744s-7.983-42.59-22.479-58.744c-5.902-6.577-5.354-16.693 1.222-22.594 6.577-5.901 16.692-5.355 22.595 1.222 19.772 22.034 30.662 50.487 30.662 80.116s-10.89 58.082-30.662 80.116c-3.159 3.521-7.527 5.314-11.913 5.314z" fill="#98d7ce"/><path d="m160 196v24c0 19.85-16.15 36-36 36h-24c-19.85 0-36-16.15-36-36v-24c0-19.85 16.15-36 36-36h24c19.85 0 36 16.15 36 36z" fill="#f8e9d2"/></g></svg>                    
+                    </div>
+                    <div class="title">
+                        <h1>Info carte Bancaire</h1>
+                    </div>
+                </div>
+                <form class="form" method="POST" action=<?php echo escape($_SERVER["PHP_SELF"]);?>>
+
+                    <label class="label">
+                        <input data-check="u_last_name" id="name" maxlength="20" class="input" type="text" name="u_last_name" value="<?php echo $user->getData()->last_name?>">
+                        <span class="border"></span>
+                        <span class="text">Name</span>
+                        <span class="error" data-error="u_last_name"></span>
+                    </label>
+                    <label class="label cardNumber">
+                        <input data-check="u_card_number" id="cardnumber" class="input" type="text" pattern="[0-9]*" inputmode="numeric"  name="u_card_number">
+                        <span class="border"></span>
+                        <span class="text">Card Number</span>
+                        <span class="error" data-error="u_card_number"></span>
+                        <svg id="ccicon" class="ccicon" width="750" height="471" viewBox="0 0 750 471" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                        </svg>
+                    </label>
+                    <div class="bottom">   
+                        <label class="label">
+                            <input data-check="u_expiration" id="expirationdate" class="input" type="text" pattern="[0-9]*" inputmode="numeric" name="u_expiration">
+                            <span class="border"></span>
+                            <span class="text">Expiration (mm/yy)</span>
+                            <span class="error" data-error="u_expiration"></span>
+                        </label>
+                        <label class="label">
+                            <input data-check="u_cvv" id="securitycode" class="input" type="text" pattern="[0-9]*" inputmode="numeric" name="u_cvv">
+                            <span class="border"></span>
+                            <span class="text">Security Code</span>
+                            <span class="error" data-error="u_cvv"></span>
+                        </label>    
+                    </div>
+                        <button class="log">Pay</button>
+            </form>
+            </div>
+        </section>
+    </main>
       
 
 
