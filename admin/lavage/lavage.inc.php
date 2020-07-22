00da34f815887895e89ba474761d15664cdc6640
Hanane-zyx <hanane.zyx@gmail.com>
2020-07-22 07:34:32 +0100

    lavage.inc

--- admin/lavage/lavage.inc.php
+++ admin/lavage/lavage.inc.php
@@ -1,26 +1,30 @@
 <?php
     require_once '/wamp64/www/PFE/core/init.php';
-if(Session::exists(('admin'))) {
+
+
+    if(Session::exists(('admin'))) {
     if(Input::exists()) {
-    	$validate = new Validate();
-        $validate->check('POST', [
-            "lavages" => ["name" => "lavages", "lavages" => Input::get('lavages')],
-            "demandes" => ["name" => "demandes", "demandes" => Input::get('demandes')]
+        if(Input::get('submit')){
+        $id = Input::get('demandes');
+        $l = new LavageMobile(Session::get('username'));
+        $lav = $l->getLavageMobile(Input::get("lavages"))->results()[0]->lavage_id;
+        $l->addDemande($lav, $id);
+        $demande = new Demande();
+        $demande->forward($id, [
+            "lavage_mobile_id" => $lav 
         ]);
 
-
-        if($validate->isValid()) {
-            $demande = new Demande();
-            $lavage = new LavgeMobile();
-            $id = $demande->getDemandes(Input::get('demandes'))->results()[0]->demande_id;
-            $demande->forward($id, [
-                "lavage_mobile_id" => $lavage->getLavageMobile(Input::get('lavages'))->results()[0]->lavage_id
-            ]);
-        }
     }
+  }
+    
+            
+    
 
     } else {
     Redirect::to("/PFE/admin/login.php");
 }
+
+
+    
 
     ?>
